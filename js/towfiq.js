ecc688e7734bf8e6fcad224f6e02638295ddd5cd
towfiq <towfiq.106@gmail.com>
2022-03-07 14:15:44 +0600

    experience updated

--- js/towfiq.js
+++ js/towfiq.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var joiningDate = "October, 1, 2018";
+    var joiningDate = "January, 1, 2018";
 
     var careerStarted = new Date(joiningDate);
     var today = new Date();
